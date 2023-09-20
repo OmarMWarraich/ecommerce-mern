d2e997ab6373acc3e67171b86a4a25baf76d5d18
OmarMWarraich <earthling986@gmail.com>
2023-09-20 14:13:02 +0500

    feat: Routes, Schemas & Cors

--- backend/app.js
+++ backend/app.js
@@ -1,69 +1,45 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv/config");
 
-require('dotenv/config');
+app.use(cors());
+app.options("*", cors());
+
+//middleware
+app.use(express.json());
+app.use(morgan("tiny"));
+
+//Routes
+const categoriesRoutes = require("./routes/categories");
+const productsRoutes = require("./routes/products");
+const usersRoutes = require("./routes/users");
+const ordersRoutes = require("./routes/orders");
 
 const api = process.env.API_URL;
 
-// Middleware
-app.use(express.json());
-app.use(morgan('tiny'));
+app.use(`${api}/categories`, categoriesRoutes);
+app.use(`${api}/products`, productsRoutes);
+app.use(`${api}/users`, usersRoutes);
+app.use(`${api}/orders`, ordersRoutes);
 
-
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-})
-
-const Product = mongoose.model('Product', productSchema);
-
-app.get(`${api}/products`, async (req, res) => {
-    const productList = await Product.find();
-
-    if(!productList) {
-        res.status(500).json({
-            success: false
-        })
-    }
-    res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: {
-            type: Number,
-            required: true
-        }
-    })
-
-    product.save().then((createdProduct => {
-        res.status(201).json(createdProduct);
-    })).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        })
-    })
-});
-
-mongoose.connect(process.env.CONNECTION_STRING, {
+//Database
+mongoose
+  .connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-    dbName: 'eshop-database'
-})
-.then(() => {
-    console.log('Database connection is ready...');
-})
-.catch((err) => {
+    dbName: "eshop-database",
+  })
+  .then(() => {
+    console.log("Database Connection is ready...");
+  })
+  .catch((err) => {
     console.log(err);
-})
+  });
 
+//Server
 app.listen(3000, () => {
-    
-    console.log('Server is running on port 3000');
+  console.log("server is running http://localhost:3000");
 });
