6d325666f5b8b10d900a427d8567704bc615721e
OmarMWarraich <earthling986@gmail.com>
2023-09-20 09:35:49 +0500

    feat: Api Call & Parse Json Data

--- backend/app.js
+++ backend/app.js
@@ -5,11 +5,25 @@ require('dotenv/config');
 
 const api = process.env.API_URL;
 
-app.get('/', (req, res) => {
-    res.send('Hello Eshop !');
+// Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+    const product = {
+        id: 1,
+        name: 'hair dresser',
+        image: 'some url'
+    }
+    res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
 });
 
 app.listen(3000, () => {
-    console.log(api);
+    
     console.log('Server is running on port 3000');
 });
