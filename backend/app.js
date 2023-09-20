619f25f573b43b4cb0d83c257f425ad47f0f5fe2
OmarMWarraich <earthling986@gmail.com>
2023-09-20 09:27:24 +0500

    feat: Env Vars

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 app.get('/', (req, res) => {
     res.send('Hello Eshop !');
 });
 
 app.listen(3000, () => {
+    console.log(api);
     console.log('Server is running on port 3000');
 });
