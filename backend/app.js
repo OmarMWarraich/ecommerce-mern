7390f35df34d5bdd4d908d847d3105f000eacd8b
OmarMWarraich <earthling986@gmail.com>
2023-09-20 10:21:36 +0500

    feat: Read/Write Data to DB w/ API

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,46 @@ const api = process.env.API_URL;
 app.use(express.json());
 app.use(morgan('tiny'));
 
+
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number
+})
+
+const Product = mongoose.model('Product', productSchema);
+
+app.get(`${api}/products`, async (req, res) => {
+    const productList = await Product.find();
+
+    if(!productList) {
+        res.status(500).json({
+            success: false
+        })
+    }
+    res.send(productList);
+});
+
+app.post(`${api}/products`, (req, res) => {
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: {
+            type: Number,
+            required: true
+        }
+    })
+
+    product.save().then((createdProduct => {
+        res.status(201).json(createdProduct);
+    })).catch((err) => {
+        res.status(500).json({
+            error: err,
+            success: false
+        })
+    })
+});
+
 mongoose.connect(process.env.CONNECTION_STRING, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -22,21 +62,6 @@ mongoose.connect(process.env.CONNECTION_STRING, {
 .catch((err) => {
     console.log(err);
 })
-
-app.get(`${api}/products`, (req, res) => {
-    const product = {
-        id: 1,
-        name: 'hair dresser',
-        image: 'some url'
-    }
-    res.send(product);
-});
-
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-});
 
 app.listen(3000, () => {
     
